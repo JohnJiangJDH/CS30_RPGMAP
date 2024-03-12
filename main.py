7272afd892a43bc15190b51537a5f411d1ae4839
JohnJiangJDH <158590134+JohnJiangJDH@users.noreply.github.com>
2024-03-12 15:31:09 +0000

    Version 1.0: Organize sections (Imports, Functions, Main), revise and improve header

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#Comments
+#####################################################################
+# Title: RPG - Dungeons Map
+# Class: Computer Science 30
+# Assignment: RPG - Map Assignment
+# Created By: John Jiang
+# Created Date: 12/03/2024
+# Version: 1.0
+######################################################################
+"""
+Creating a map for a simple game. As the player moves through the map,
+descriptions of the different locations that the user is located in
+will be shown. 
+"""
+######################################################################
+# IMPORTS AND GLOBAL VARIABLES ---------------------------------------
+
+
+
+######################################################################
+# FUNCTIONS ---------------------------------------
+
+
+
+######################################################################
+# MAIN ---------------------------------------
