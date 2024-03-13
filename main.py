a1fa2832267b8f7ef41a0b4fd9a307bfcf619706
JohnJiangJDH <158590134+JohnJiangJDH@users.noreply.github.com>
2024-03-13 15:30:20 +0000

    Began menus with user input, added global variables and dictionaries

--- main.py
+++ main.py
@@ -14,12 +14,59 @@ will be shown.
 ######################################################################
 # IMPORTS AND GLOBAL VARIABLES ---------------------------------------
 
+loop = True
+
+movementOptions = ["Walk", "Swim", "Fly", "Quit"]
+
+#player = {xLoc: 0, }
+
+dungeon_rooms = {"Starting Room": {"Description": "You have entered\
+some ruins and the exploration of a mysterious dungeon awaits you. ",
+                                 "Options": ["left", "right"]}}
+
+
+# dungeon_map = [[startFloor, startRoom],
+#                [startFloor, staffRoom],
+#                [startFloor, bookRoom],
+#                [startFloor, upgradeRoom]]
+
 
 
 ######################################################################
 # FUNCTIONS ---------------------------------------
 
 
+def mainMenu():
+    try:
+        count = 0
+        for option in movementOptions:
+            if count <= len(movementOptions):
+                count += 1
+                print(f"{count}) {option}")
+                
+        movement = int(input("Choose a movement option: "))
+        return movementOptions[movement-1]
+        
+    except Exception:
+        print("There was an error.")
+
+
+def main():
+    global loop
+    try:
+        while loop:
+            test = mainMenu()
+            if not test is None or test != "Quit":
+                print(test)
+                loop = False
+            else:
+                print("Try again.")
+    
+    except Exception:
+        print("There was an error.")
+        
 
 ######################################################################
 # MAIN ---------------------------------------
+
+main()
